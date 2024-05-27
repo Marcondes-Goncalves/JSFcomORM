a20615c39a71ad92bd8e9999a8381ee9001aae3d
Marcondes <marcondesgoncalves159@gmail.com>
2024-05-27 18:40:49 -0300

    Projeto criado

---
+++ src/main/java/com/marcondes/PessoaBean.java
@@ -0,0 +1,60 @@
+package com.marcondes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+@ManagedBean(name = "pessoaBean")
+@ViewScoped
+public class PessoaBean {
+    
+    private String nome;
+
+    private HtmlCommandButton commandButton;
+
+    private List<String> nomes = new ArrayList<String>();
+
+    public String addNome(){
+        nomes.add(nome);
+
+        if (nomes.size() > 3) {
+            commandButton.setDisabled(true);
+        }
+
+        return "";
+    }
+
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(List<String> nomes) {
+        this.nomes = nomes;
+    }
+
+
+
+    public HtmlCommandButton getCommandButton() {
+        return commandButton;
+    }
+
+
+
+    public void setCommandButton(HtmlCommandButton commandButton) {
+        this.commandButton = commandButton;
+    }
+
+}
